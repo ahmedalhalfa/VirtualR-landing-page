25568a0bb3ac2e1ae559b7b071a0aa0b9aa74b06
ahmed alhalfa <ah.alhalfa@gmail.com>
2024-10-09 15:43:32 +0300

    made videos load online instead of locally

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,3 @@
-import video1 from '../assets/video1.mp4';
-import video2 from '../assets/video2.mp4';
-
 const HeroSection = () => {
   return (
     <div className="flex flex-col items-center mt-6 lg:mt-20">
@@ -25,11 +22,11 @@ const HeroSection = () => {
       </div>
       <div className="flex mt-10 justify-center">
         <video autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4">
-          <source src={video1} type="video/mp4" />
+          <source src="https://i.imgur.com/jCiPYrV.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
         <video autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4">
-          <source src={video2} type="video/mp4" />
+          <source src="https://i.imgur.com/b3Nj2ys.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
